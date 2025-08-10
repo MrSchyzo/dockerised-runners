4831d1773bf9cf90cf120f6b249be425facbf78f
Mr.Schyzo <16671172+MrSchyzo@users.noreply.github.com>
2025-08-10 02:32:32 +0200

    More fixes

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Relies on REPO and TOKEN being defined
-cd /home/docker/actions-runner
+# Relies on REPO and ACCESS_TOKEN being defined
 
-./config.sh --url https://github.com/$REPO --token $TOKEN
+TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cleanup() {
     echo "Removing runner..."
     ./config.sh remove --token $TOKEN
 }
-
 trap cleanup SIGTERM SIGINT SIGQUIT
+
+cd /home/docker/actions-runner
+
+./config.sh --url https://github.com/$REPO --token $TOKEN
 
 ./run.sh
